b4b38a2568a293dd240704fc10ef7ae4efe91162
dimamedv <dimamedv1@gmail.com>
2022-07-28 13:12:49 +0300

    Initial commit

---
+++ Assets/Skripts/MyMath.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Skripts
+{
+    public class MyMath
+    {
+        public Vector3 CalculateMidPoint(Vector3 v1, Vector3 v2)
+        {
+            return v1 + (v2 - v1) / 2;
+        }
+    }
+}
