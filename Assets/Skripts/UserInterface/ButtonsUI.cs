60bb77d3c3e432e7e2f1daf578c27f6a66eaebb2
ChuvilkoDEV <ilyasutton@gmail.com>
2022-10-20 23:50:58 +0300

    Поправил UI

--- Assets/Skripts/UserInterface/ButtonsUI.cs
+++ Assets/Skripts/UserInterface/ButtonsUI.cs
@@ -19,16 +19,35 @@ public class ButtonsUI : MonoBehaviour
     {
         isStraight = !isStraight;
 
-        Transform straightImage = gameObject.transform.Find("Canvas").Find("Type").Find("Toggle");
+        Transform imageTransform = gameObject.transform.Find("Canvas").Find("Panel").Find("Type").Find("Toggle");
         if (isStraight)
         {
-            straightImage.Rotate(new Vector3(0.0f, 0.0f, 180.0f));
+            imageTransform.Rotate(new Vector3(0.0f, 0.0f, 180.0f));
             roadCreator.ButtonStraightIsPressed();
         }
         else
         {
 
-            straightImage.Rotate(new Vector3(0.0f, 0.0f, -180.0f));
+            imageTransform.Rotate(new Vector3(0.0f, 0.0f, -180.0f));
+            roadCreator.ButtonCrookedIsPressed();
+        }
+    }
+
+    private bool renderMesh = true;
+    public void ChangeRender()
+    {
+        renderMesh = !renderMesh;
+
+        Transform imageTransform = gameObject.transform.Find("Canvas").Find("Panel").Find("Render").Find("Toggle");
+        if (renderMesh)
+        {
+            imageTransform.Rotate(new Vector3(0.0f, 0.0f, 180.0f));
+            roadCreator.ButtonStraightIsPressed();
+        }
+        else
+        {
+
+            imageTransform.Rotate(new Vector3(0.0f, 0.0f, -180.0f));
             roadCreator.ButtonCrookedIsPressed();
         }
     }
